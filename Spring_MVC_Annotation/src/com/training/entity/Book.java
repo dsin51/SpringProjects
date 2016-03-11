a312e432432684afddc5ccd96ea6073ebed36191
dsin51 <dsinghal3@sapient.com>
2016-03-11 14:57:04 +0530

    check

--- Spring_MVC_Annotation/src/com/training/entity/Book.java
+++ Spring_MVC_Annotation/src/com/training/entity/Book.java
@@ -1,29 +1,36 @@
 package com.training.entity;
 
+import java.util.logging.Logger;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class Book {
 
+	Logger log = Logger.getLogger(this.getClass().getName());
+
 	private long bookNumber;
 	private String bookName;
+
 	public Book() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+
 	public long getBookNumber() {
 		return bookNumber;
 	}
+
 	public void setBookNumber(long booknumber) {
 		this.bookNumber = booknumber;
 	}
+
 	public String getBookName() {
 		return bookName;
 	}
+
 	public void setBookName(String bookName) {
 		this.bookName = bookName;
 	}
-	
-	
-	
+
 }
