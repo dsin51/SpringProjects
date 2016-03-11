e43a40280de9b1dd3787113707b11830c174d3f8
dsin51 <dsinghal3@sapient.com>
2016-03-11 11:23:13 +0530

    config

---
+++ SpringMVC/src/com/training/control/FirstController.java
@@ -0,0 +1,24 @@
+package com.training.control;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class FirstController implements Controller {
+
+	@Autowired
+	private ModelAndView mdl;
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest arg0, HttpServletResponse arg1) throws Exception {
+
+		mdl.setViewName("Success");
+		String message = "You have configured MVC correctly!!";
+		mdl.addObject("msg", message);
+		return mdl;
+	}
+
+}
