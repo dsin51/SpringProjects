d62c6710b5fcd9dfd8190ff24578e14c417d0844
dsin51 <dsinghal3@sapient.com>
2016-03-17 15:45:03 +0530

    dao

---
+++ FirstHibernateExample/src/com/training/daos/DoctorDAO.java
@@ -0,0 +1,40 @@
+package com.training.daos;
+
+import java.util.List;
+
+import com.training.ifaces.MyDAO;
+
+public class DoctorDAO<T> implements MyDAO<T> {
+
+	@Override
+	public Object add(T t) {
+
+		
+		return null;
+	}
+
+	@Override
+	public T find(Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<T> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T update(T obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean delete(Object obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
