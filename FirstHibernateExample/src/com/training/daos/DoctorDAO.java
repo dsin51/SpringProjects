a224fa2985ffb5566a1051bb338bc394cefd6fc1
dsin51 <dsinghal3@sapient.com>
2016-03-17 16:08:06 +0530

    add works

--- FirstHibernateExample/src/com/training/daos/DoctorDAO.java
+++ FirstHibernateExample/src/com/training/daos/DoctorDAO.java
@@ -2,31 +2,42 @@ package com.training.daos;
 
 import java.util.List;
 
+import org.hibernate.*;
+
+import com.training.entity.Doctor;
 import com.training.ifaces.MyDAO;
+import com.training.utils.HiberUtils;
 
-public class DoctorDAO<T> implements MyDAO<T> {
+public class DoctorDAO<Doctor> implements MyDAO<Doctor> {
 
+	SessionFactory factory = HiberUtils.getFactory();
+	
 	@Override
-	public Object add(T t) {
+	public Object add(Doctor doc) {
 
-		
-		return null;
+		Session session = factory.openSession();
+
+		Transaction tx = session.beginTransaction();
+
+		session.save(doc);
+		tx.commit();
+		return doc;
 	}
 
 	@Override
-	public T find(Object obj) {
+	public Doctor find(Object obj) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public List<T> findAll() {
+	public List<Doctor> findAll() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public T update(T obj) {
+	public Doctor update(Doctor obj) {
 		// TODO Auto-generated method stub
 		return null;
 	}
